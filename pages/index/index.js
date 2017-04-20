64541d51a162a3e735cb338d3accd3b0f5c78250
yangxinbendi <1373219495@qq.com>
2017-04-20 21:12:22 +0800

    weixinApp

---
+++ pages/index/index.js
@@ -0,0 +1,14 @@
+//index.js
+//获取应用实例
+var app = getApp()
+Page({
+  data: {
+    list:[{type:'猪肉',money:'100',name:'张三'},
+    {type:'牛肉',money:'200',name:'李四'},
+    {type:'猪腰',money:'300',name:'王五'},
+    {type:'羊肉',money:'400',name:'赵六'}]
+  },
+  onLoad: function () {
+
+  }
+})
